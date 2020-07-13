a0ea017d33023db37977084bf62bee63898efd27
Sven Duve <svenduve@gmail.com>
2020-07-13 10:13:03 +0200

    Some more updates

--- src/MeinBeispiel.jl
+++ src/MeinBeispiel.jl
@@ -1,11 +1,13 @@
 module MeinBeispiel
 
+import ForwardDiff
+
 greet() = println("Hallo du Pfeife")
 # Write your package code here.
 
 include("meineFunktion.jl")
 
 
-export meineFunktion
+export meineFunktion, derivativat_von_meineFunktion
 
 end
--- src/meineFunktion.jl
+++ src/meineFunktion.jl
@@ -1,5 +1,7 @@
 function meineFunktion(x, y)
 
-    x + y
+    2x^3 + y
 
 end
+
+derivativat_von_meineFunktion(x, y) = ForwardDiff.derivative(x -> meineFunktion(x, y), x)
